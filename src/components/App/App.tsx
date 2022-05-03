cc35d55d637790ce7c57ce06ee414c7cceadd064
Bacorn Liafiaho <kacper123987@gmail.com>
2022-05-03 01:06:29 -0400

    Created basic UI to enter and translate code

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello!</div>;
+  const [code, setCode] = useState("");
+  const [translatedCode, setTranslatedCode] = useState("");
+
+  const changeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setCode(e.target.value);
+  };
+
+  const clickHandler = () => {
+    setTranslatedCode(translate(code));
+  };
+
+  const translate = (code: string): string => {
+    return code;
+  };
+
+  return (
+    <div className="App">
+      Hello!
+      <div className="editor">
+        <textarea value={code} onChange={changeHandler} />
+        <button onClick={clickHandler}>Translate</button>
+        <textarea value={translatedCode} readOnly={true} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
